cb4fe2206de72dce782456db5dd7cf9a6013f973
Leonardo Vilela Teixeira <leolvt@gmail.com>
2012-07-27 12:39:57 -0300

    Change the login message
    Change the colors and correct the uptime message.

--- login/motd.sh
+++ login/motd.sh
@@ -2,31 +2,41 @@
 #define the filename to use as output
 #motd="~/.login/motd"
 # Collect useful information about your system
-# $USER is automtically defined
+# $USER is automatically defined
 HOSTNAME=`uname -n`
 KERNEL=`uname -r`
 CPU=`uname -p`
 ARCH=`uname -m`
 DATELINE=`~/.login/data_hora.pl 1`
-UP=$(uptime | sed -e 's/.*up\(.*\)/up&1/')
+UP=$(uptime | sed -e 's/.*up\(.*\)/Up\1/')
 # The different colours as variables
-W="\033[01;37m"
-Y="\033[01;30m"
-B="\033[01;34m"
-G="\033[01;32m"
-P="\033[01;35m"
-R="\033[01;31m" 
-X="\033[00;37m"
+gray="\033[00;30m"
+GRAY="\033[01;30m"
+red="\033[00;31m"
+RED="\033[01;31m"
+green="\033[00;32m"
+GREEN="\033[01;32m"
+brown="\033[00;33m"
+BROWN="\033[01;33m"
+blue="\033[00;34m"
+BLUE="\033[01;34m"
+purple="\033[00;35m"
+PURPLE="\033[01;35m"
+cyan="\033[00;36m"
+CYAN="\033[01;36m"
+white="\033[00;37m"
+WHITE="\033[01;37m"
+NC="\033[00m" # No Color
+
 clear # to clear the screen when showing up
-echo -e "$R#=============================================================================#"
+echo -e "${RED}#=============================================================================#"
 echo -e ""
-echo -e "   $G $DATELINE"
+echo -e "    ${GREEN}${DATELINE}"
 echo -e ""
-echo -e "   $R ARCH    $W= $ARCH $B|$R KERNEL    $W= $KERNEL             "
-echo -e "   $W $UP$W"
+echo -e "    ${RED}ARCH ${WHITE}= ${BROWN}${ARCH}    ${PURPLE}|    ${RED}KERNEL ${WHITE}= ${BROWN}${KERNEL}"
+echo -e "    ${cyan}${UP}"
 echo -e ""
-echo -e "   $W Bem-vindo, $B $USER $W to $B $HOSTNAME                "
+echo -e "    ${WHITE}Bem-vindo,${BLUE} ${USER}${WHITE} to${green} ${HOSTNAME}"
 echo -e ""
-echo -e "$R#=============================================================================#$X"
-#echo -e "$X"
+echo -e "${RED}#=============================================================================#${NC}"
 
