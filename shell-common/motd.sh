01f285ef99d0171aaf0f3b034cef9d3415dade29
Leonardo Vilela Teixeira <leolvt@gmail.com>
2013-07-13 16:02:27 -0300

    Improvement on MotD
    Add last and current login times to the message of the day and reorganize it a
    bit.

--- shell-common/motd.sh
+++ shell-common/motd.sh
@@ -27,18 +27,33 @@ white="\033[00;37m"
 WHITE="\033[01;37m"
 NC="\033[00m" # No Color
 
+# last and current login info
+currdate=`last $USER | grep "still" | egrep ":0\s+:0" | head -n 1 | awk '{print $4,$5,$6}'`
+currtime=`last $USER | grep "still" | egrep ":0\s+:0" | head -n 1 | awk '{print $7}'`
+lastdate=`last $USER | grep -v "still" | egrep ":0\s+:0" | head -n 1 | awk '{print $4,$5,$6}'`
+laststart=`last $USER | grep -v "still" | egrep ":0\s+:0" | head -n 1 | awk '{print $7}'`
+lastend=`last $USER | grep -v "still" | egrep ":0\s+:0" | head -n 1 | awk '{print $9}'`
+lastdur=`last $USER | grep -v "still" | egrep ":0\s+:0" | head -n 1 | awk '{print $10}'`
+lastdur=${lastdur:1:5}
+
+echo "Logged in since: $currdate $currtime"
+echo ""
+echo "    Last login: $lastdate << $laststart >> => << $lastend >> $lastdur"
 # Generate the MOTD if needed
 perl ~/.config/shell-common/motd-gen.pl
 
 clear # to clear the screen when showing up
 echo -e "${RED}#=============================================================================#"
 echo -e ""
-echo -e "    ${GREEN}${DATELINE}"
+echo -e "    ${WHITE}Bem-vindo,${BLUE} ${USER}${WHITE} - Host:${green} ${HOSTNAME}"
 echo -e ""
 echo -e "    ${RED}ARCH ${WHITE}= ${BROWN}${ARCH}    ${PURPLE}|    ${RED}KERNEL ${WHITE}= ${BROWN}${KERNEL}"
+echo -e ""
+echo -e "    Data/Hora atual: ${GREEN}${DATELINE}"
 echo -e "    ${cyan}${UP}"
 echo -e ""
-echo -e "    ${WHITE}Bem-vindo,${BLUE} ${USER}${WHITE} to${green} ${HOSTNAME}"
+echo -e "    ${CYAN}Logado desde: ${BLUE} $currdate${blue}, ${BLUE}$currtime"
+echo -e "    ${CYAN}Último login: ${BLUE} $lastdate${blue}, ${BLUE}$laststart ${blue}- ${BLUE}$lastend  ${CYAN}Duração: ${BLUE}$lastdur"
 echo -e ""
 echo -e "${GREEN}#-----------------------------------------------------------------------------#${NC}"
 echo -e "\n${WHITE}Fortune of the day: \n${NC}"
